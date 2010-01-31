bd4587854249714e8ae371880624b412b7898a35
Lox <lox.dev@knc.nc>
2010-01-31 16:25:38 +1100

    Updated backup script

--- tools/releasetools/backuptool.sh
+++ tools/releasetools/backuptool.sh
@@ -45,69 +45,86 @@ case "$1" in
         mount /cache
         mount /data
         
-        backup_file /system/app/EnhancedGoogleSearchProvider.apk
-        backup_file /system/app/Facebook.apk
-        backup_file /system/app/Gmail.apk
-        backup_file /system/app/GenieWidget.apk
-        backup_file /system/app/GmailProvider.apk
-        backup_file /system/app/GoogleApps.apk
-        backup_file /system/app/GoogleBackupTransport.apk
-        backup_file /system/app/GoogleCheckin.apk
-        backup_file /system/app/GoogleContactsSyncAdapter.apk
-        backup_file /system/app/GooglePartnerSetup.apk
-        backup_file /system/app/GoogleSettingsProvider.apk
-        backup_file /system/app/GoogleSubscribedFeedsProvider.apk
+        backup_file /system/app/HtcCopyright.apk
+        backup_file /system/app/CarDock.apk
+        backup_file /system/app/Vending.apk
         backup_file /system/app/gtalkservice.apk
+        backup_file /system/app/VoiceSearchWithKeyboard.apk
+        backup_file /system/app/GoogleCheckin.apk
+        backup_file /system/app/GenieWidget.apk
+        backup_file /system/app/GoogleApps.apk
+        backup_file /system/app/Facebook.apk
+        backup_file /data/app/Facebook.apk
+        backup_file /system/app/GoogleSubscribedFeedsProvider.apk
+        backup_file /system/app/GoogleContactsSyncAdapter.apk
+        backup_file /system/app/MarketUpdater.apk
+        backup_file /system/app/BugReport.apk
+        backup_file /system/app/YouTube.apk
         backup_file /data/app/Maps.apk
         backup_file /system/app/Maps.apk
-        backup_file /system/app/MarketUpdater.apk
-        backup_file /system/app/MediaUploader.apk
+        backup_file /system/app/GoogleBackupTransport.apk
+        backup_file /system/app/Gmail.apk
+        backup_file /system/app/EnhancedGoogleSearchProvider.apk
+        backup_file /system/app/GmailProvider.apk
+        backup_file /system/app/com.amazon.mp3.apk
+        backup_file /data/app/com.amazon.mp3.apk
         backup_file /system/app/NetworkLocation.apk
-        backup_file /system/app/SetupWizard.apk
         backup_file /system/app/Street.apk
         backup_file /system/app/Talk.apk
         backup_file /system/app/TalkProvider.apk
-        backup_file /system/app/TtsService.apk
-        backup_file /system/app/Vending.apk
-        backup_file /system/app/VoiceSearchWithKeyboard.apk
-        backup_file /system/app/YouTube.apk
+        backup_file /system/app/googlevoice.apk
+        backup_file /data/app/googlevoice.apk
+        backup_file /system/app/LatinImeTutorial.apk
+        backup_file /system/app/SetupWizard.apk
+        backup_file /system/app/GooglePartnerSetup.apk
+        backup_file /system/app/MediaUploader.apk
+        backup_file /system/app/PassionQuickOffice.apk
+        backup_file /system/app/GoogleSettingsProvider.apk
         backup_file /system/etc/permissions/com.google.android.gtalkservice.xml
         backup_file /system/etc/permissions/com.google.android.maps.xml
+        backup_file /system/framework/com.google.android.datamessaging.xml
         backup_file /system/framework/com.google.android.gtalkservice.jar
         backup_file /system/framework/com.google.android.maps.jar
         backup_file /system/lib/libgtalk_jni.so
         backup_file /system/lib/libspeech.so
         ;;
     restore)
-        restore_file /system/app/EnhancedGoogleSearchProvider.apk
-        restore_file /system/app/Facebook.apk
-        restore_file /system/app/Gmail.apk
-        restore_file /system/app/GenieWidget.apk
-        restore_file /system/app/GmailProvider.apk
-        restore_file /system/app/GoogleApps.apk
-        restore_file /system/app/GoogleBackupTransport.apk
-        restore_file /system/app/GoogleCheckin.apk
-        restore_file /system/app/GoogleContactsSyncAdapter.apk
-        restore_file /system/app/GooglePartnerSetup.apk
-        restore_file /system/app/GoogleSettingsProvider.apk
-        restore_file /system/app/GoogleSubscribedFeedsProvider.apk
+        restore_file /system/app/HtcCopyright.apk
+        restore_file /system/app/CarDock.apk
+        restore_file /system/app/Vending.apk
         restore_file /system/app/gtalkservice.apk
-        restore_file /data/app/Maps.apk
+        restore_file /system/app/VoiceSearchWithKeyboard.apk
+        restore_file /system/app/GoogleCheckin.apk
+        restore_file /system/app/GenieWidget.apk
+        restore_file /system/app/GoogleApps.apk
+        restore_file /data/app/Facebook.apk
+        restore_file /system/app/GoogleSubscribedFeedsProvider.apk
+        restore_file /system/app/GoogleContactsSyncAdapter.apk
         restore_file /system/app/MarketUpdater.apk
-        restore_file /system/app/MediaUploader.apk
+        restore_file /system/app/BugReport.apk
+        restore_file /system/app/YouTube.apk
+        restore_file /data/app/Maps.apk
+        restore_file /system/app/GoogleBackupTransport.apk
+        restore_file /system/app/Gmail.apk
+        restore_file /system/app/EnhancedGoogleSearchProvider.apk
+        restore_file /system/app/GmailProvider.apk
+        restore_file /data/app/com.amazon.mp3.apk
         restore_file /system/app/NetworkLocation.apk
-        restore_file /system/app/SetupWizard.apk
         restore_file /system/app/Street.apk
         restore_file /system/app/Talk.apk
         restore_file /system/app/TalkProvider.apk
-        restore_file /system/app/TtsService.apk
-        restore_file /system/app/Vending.apk
-        restore_file /system/app/VoiceSearchWithKeyboard.apk
-        restore_file /system/app/YouTube.apk
+        restore_file /data/app/googlevoice.apk
+        restore_file /system/app/LatinImeTutorial.apk
+        restore_file /system/app/SetupWizard.apk
+        restore_file /system/app/GooglePartnerSetup.apk
+        restore_file /system/app/MediaUploader.apk
+        restore_file /system/app/PassionQuickOffice.apk
+        restore_file /system/app/GoogleSettingsProvider.apk
         restore_file /system/etc/permissions/com.google.android.gtalkservice.xml
         restore_file /system/etc/permissions/com.google.android.maps.xml
         restore_file /system/framework/com.google.android.gtalkservice.jar
         restore_file /system/framework/com.google.android.maps.jar
+        restore_file /system/framework/com.google.android.datamessaging.xml
         restore_file /system/lib/libgtalk_jni.so
         restore_file /system/lib/libspeech.so
         ;;
@@ -117,4 +134,3 @@ case "$1" in
 esac
 
 exit 0
-
